0a4367f5d7899bb50f134ff0ae4ea429d1dd8821
Tim Riffe <tim.riffe@gmail.com>
2016-03-07 12:59:40 +0100

    edits passed on to Alyson

--- ATL_SexDecomp/R/ATL.R
+++ ATL_SexDecomp/R/ATL.R
@@ -12,4 +12,19 @@ ramp <-
 names()
 X <- Dat[["SRHpoor"]]$Females$Surf[,,3]
 
+source("/home/tim/git/ATL_SexDecomp/ATL_SexDecomp/R/SurfMap.R")
+ticks <- seq(0,.8,by=.1)
+graphics.off()
+dev.new(width=7,height=4.2)
+pdf("Figures/SRHpoor_Females.pdf",width=7,height=4.2)
+SurfMap(Dat[["IADL1"]]$Females$Surf[,,3], ticks = ticks, bg=TRUE,ylab="time-to-death",xlab="age",
+		mai=c(.6,.5,.4,1.2))
+text(103,16,"prevalence")
+dev.off()
+pdf("Figures/IADL1_Males.pdf",width=7,height=4.2)
+SurfMap(Dat[["IADL1"]]$Males$Surf[,,3], ticks = ticks, bg=TRUE,ylab="time-to-death",xlab="age",
+		mai=c(.6,.5,.4,1.2))
+text(103,16,"prevalence")
+dev.off()
 
+names(Dat)
