71e3a6788a7b334784a6e3df60a1f4e35ab80f7e
Tim Riffe <tim.riffe@gmail.com>
2018-02-20 15:18:29 +0100

    edits through first 2 paragraphs of section 2

--- R/4_ATL.R
+++ R/4_ATL.R
@@ -45,7 +45,7 @@ ttdlim  <- range(ttd) + c(0,1)
 agelim  <- range(ages) + c(0,1)
 
 agetoin <- .3
-mai     <- c(.6, .6, .1, .1)
+mai     <- c(.6, .7, .1, .1)
 mh      <- sum(mai[c(1,3)])
 mw      <- sum(mai[c(2,4)])
 nages   <- diff(agelim)
@@ -54,6 +54,9 @@ nthan   <- diff(ttdlim)
 height  <- nthan * agetoin + mh
 
 wnarrow <- nthan * agetoin + mw
+
+ylabspace <- -1.9
+xlabat    <- -1.5
 
 ymax    <- max(pretty(XS))
 pdf("Figures/srhpoor_f_Surf_a.pdf", width = wwide, height = height)
@@ -67,8 +70,8 @@ plot(NULL, type = "n",
 		asp = 1)
 SurfMap(XS, ticks = ticks, bg = TRUE, ylab = "", xlab = "",
 		legnd = FALSE, add = TRUE)
-text(80, -1.5, "Age", cex = 1.2)
-text(68.5, 7, "TTD", cex = 1.2, srt = 90)
+text(80, xlabat, "Age", cex = 1.2)
+text(min(ages) + ylabspace, mean(ttd), "TTD", cex = 1.2, srt = 90)
 dev.off()
 
 # make the TTD over age lines
@@ -103,22 +106,21 @@ text(ages[ages %% 5 == 0],-.02,ages[ages %% 5 == 0],pos=1,cex=.8)
 yvals <- ncol(XS) - col(XS) + 1 == row(XS)
 TTDs  <- c(12:1,"TTD = 0")
 keep  <- c(TRUE,FALSE,FALSE,TRUE,FALSE,FALSE,TRUE,FALSE,FALSE,TRUE,FALSE,FALSE)
-text((83:96)[keep]-5,XS[yvals][keep]-.01,TTDs[keep],pos=c(4,4,4,4,1),cex=.8)
+text((83:96)[keep] - 5, XS[yvals][keep]-.01, TTDs[keep], pos = c(4,4,4,4,1), cex = .8)
 
-text(80,-.058,"Age", cex = 1.2)
-text(68.25, .25, "Prevalence", cex = 1.2,srt=90)
+text(80, -.058, "Age", cex = 1.2)
+text(min(ages) + ylabspace, .25, "Prevalence", cex = 1.2,srt=90)
 dev.off()
-# trick to get x dims to match.
+
 
 
 # make the age over TTD lines
 ttdcols <- colorRampPalette(brewer.pal(9,"Greens")[-c(1:3)])
-#dev.new(width=((7 - .7) / diff(c(70,max(ages)+1))) * 13 + .7,height=5)
-XSS <- XS[, as.integer(colnames(XS))%%5 == 0]
+
 pdf("Figures/srhpoor_f_TTD_b.pdf",
 		width = wnarrow,
 		height = height)
-par(mai= mai, xaxs = "i", yaxs = "i", xpd = TRUE)
+par(mai = mai, xaxs = "i", yaxs = "i", xpd = TRUE)
 plot(NULL, type = "n", 
 		xlim = ttdlim,
 		ylim = c(0, ymax),
@@ -151,9 +153,11 @@ yvals <- XS[,keep][c(13,26,37,45)]
 xvals <- c(12,12,10,5)
 text(xvals,yvals,c(75,80,85,"Age = 90"),pos=c(4,1,1,1),cex=.8)
 text(6, -.045, "TTD")
-text(-1.75,.25,"Prevalence",srt=90, cex = 1.2)
+text(min(ttd)+ylabspace, .25, "Prevalence", srt=90, cex = 1.2)
 dev.off()
 
+# a blank filler graph so that spacing works out in latex...
+# this possibly won't be necessary later on.
 pdf(
 		"Figures/blankholder.pdf", 
 		width = wnarrow,
@@ -168,7 +172,10 @@ plot(
 		ylab = "")
 dev.off()
 
+# -----------------------------------------
 # another version of TTD graph for presentation:
+do.this <- FALSE
+if (do.this){
 pdf("Figures/srhpoor_f_TTD_b2.pdf",
 		width=((7 - .7) / diff(c(70,max(ages)+1))) * 13 + .7,
 		height=5)
@@ -201,6 +208,7 @@ text(-1.75,.25,"Prevalence",srt=90, cex = 1.2)
 lines(0:12,rowMeans(XS[,colSums(!is.na(XS))>7],na.rm=TRUE),lwd=3,col="red")
 text(8,.25,"Average TTD prevalence")
 dev.off()
+}
 #c(7,((7 - .7) / diff(c(70,101))) * 13 + .7) * 1.2 / 10
 
 #ticks <- seq(0,.8,by=.1)
